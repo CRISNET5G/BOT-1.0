3294b71f8b1a06c1e2addcc900efb78810fd6c49
MASTER VIP <109626455+CRISNET5G@users.noreply.github.com>
2022-11-08 22:32:37 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 while : 
 do
 echo -e "\n\033[1;31m[ \033[1;33mATENÇÃO! \033[1;31m]\033[1;37m - \033[1;32mVOCÊ PRECISARÁ DE UM SEGUNDO CELULAR PARA ESCANEAR \033[0m"
-echo -e "\n\033[1;31m[ \033[1;33mACESSE NOSSO CANAL NO TELEGRAM \033[1;31m]\033[1;37m - \033[1;32m@GESTORVIPCANAL \033[0m"
+echo -e "\n\033[1;31m[ \033[1;33mACESSE NOSSO CANAL NO TELEGRAM \033[1;31m]\033[1;37m - \033[1;32m \033[0m"
 echo -e "\n\033[1;31m \033[1;33m⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
 ████████████████████████████████████████
 ████████████████████████████████████████
@@ -29,4 +29,4 @@ MASTER VIP 1.0"
     node index.js
     sleep 1
 
-done
+done 
